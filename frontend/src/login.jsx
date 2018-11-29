21d166226331aeacda42f366c40862d992164ebf
tiiarautavesi <tiia.rautavesi@metropolia.fi>
2018-11-29 22:23:05 +0200

    fix the damn modal

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -9,11 +9,26 @@ class Login extends React.Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      show: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+  
+  showModal(event) {
+    this.setState({ 
+      show: true 
+    });
+  }
+  
+  hideModal(event) {
+    this.setState({ 
+      show: false 
+    });
   }
 
   handleChange(event) {
@@ -22,21 +37,13 @@ class Login extends React.Component {
     const name = target.name;
     
     this.setState({
-      [name]: value,
-      isHidden: true
+      [name]: value
     });
   }
+  
+    
+  
 
-  onClick() {
-      this.setState({ showResults: true });
-  }
-  
-  toggleHidden() {
-    this.setState({
-      isHidden: this.state.isHidden
-    })
-  }
-  
   handleSubmit(event) {
     console.log('A name was submitted: ' + this.state.username + '. A password was submitted: ' + this.state.password);
     event.preventDefault(); 
@@ -53,20 +60,28 @@ class Login extends React.Component {
           <input name="password" className="login-input" type="password" value={this.state.password} onChange={this.handleChange} />
           <input className="login-button" type="submit" value="Kirjaudu" onClick={() => this.props.action()} />
         </form>
-        <a className="login-issue" onClick={this.toggleHidden.bind(this)}>Minulla ei vielä ole tunnuksia OPPE:en</a>
-        {this.state.isHidden && <NewUserInfo />}
+        <a className="login-issue" onClick={this.showModal}>
+          Minulla ei vielä ole tunnuksia OPPE:en
+        </a>
+        <NewUserInfo show={this.state.show} handleClose={this.hideModal} />
+        {console.log(this.state.show)}
         
       </div>
-    );
+    )
   }
 }
 
-const NewUserInfo = () => (
-  <div className="new-user-info">
-    <div className="close" onClick={this.toggleHidden.bind(this)}>x</div>
-    Ihanaa, että olet kiinnostunut OPPE:sta! Ota yhteyttä oppilaitoksesi tietohallintoon, tai tilaa henkilökohtainen demo
-    <a target="_blank" href=""> tästä</a>.
-  </div>
-)
+const NewUserInfo = ({handleClose, show}) => {
+  const showHideClassName = show ? 'new-user-info display-block' : 'new-user-info display-none';
+  return (
+    <div>
+      <div className={showHideClassName}>
+        <div className="close" onClick={handleClose}>x</div>
+        Ihanaa, että olet kiinnostunut OPPE:sta! Ota yhteyttä oppilaitoksesi tietohallintoon, tai tilaa henkilökohtainen demo
+        <a target="_blank" href=""> tästä</a>.
+      </div>
+    </div>
+  );
+}
 
 export default Login;
